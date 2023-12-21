001b0bbf697f95a6d9ab37910a155e477e45f7a6
Edwin <>
2023-12-21 14:47:03 -0800

    update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,43 @@
 import express from "express";
 
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+
 const app = express();
 
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+const main = async () => {
+  await server.start();
+  app.use(cors(), bodyParser.json(), expressMiddleware(server));
+};
+
+main();
+
 app.get("/api/test", (req, res) => {
-  res.end(`hello world`);
+  res.end(`hello world1`);
 });
 
 export default app;
